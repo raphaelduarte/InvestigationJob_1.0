0073e822a165be5266d1d82a376b0607e8bbd226
Raphael Duarte <raphaelduartessph@gmail.com>
2023-04-09 17:06:44 -0300

    Primeiro Commit

---
+++ Investigation Job 1.0 - ServerApp/Pages/Marketplace.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Investigation_Job_1._0___ServerApp.Pages;
+
+public class Marketplace : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
